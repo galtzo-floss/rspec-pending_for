27deaed6c8d1f948c5546101edaf40cca789e394
Peter H. Boling <peter.boling@gmail.com>
2026-07-28 13:27:36 -0600

    🔖 Bump gem version

--- lib/rspec/pending_for/version.rb
+++ lib/rspec/pending_for/version.rb
@@ -5,7 +5,7 @@ module Rspec
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "0.1.22"
+      VERSION = "0.1.23"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
